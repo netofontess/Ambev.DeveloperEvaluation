6cf47f8ebb2ed8dd0ffc4f5c77dc6c4ada1aa7fb
Neto Fontes <netofontes@digix.com.br>
2024-12-16 21:10:19 -0300

    - Remoção using sobrando;

--- template/backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/SaleTests.cs
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/SaleTests.cs
@@ -1,10 +1,5 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Entities;
 using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities
